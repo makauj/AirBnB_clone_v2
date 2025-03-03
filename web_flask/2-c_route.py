16171ef7782d2fb729dec84446932477a2c35da5
makau <makauwanyoike@gmail.com>
2025-03-04 01:11:46 +0300

    docstring?

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -8,16 +8,19 @@ app = Flask(__name__)
 
 @app.route("/c/<text>", strict_slashes=False)
 def display_c(text):
+    """display C followed by the value of the text variable"""
     return f"C {text.replace("_", " ")}"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
+    """hbnb route"""
     return "HBNB"
 
 
 @app.route("/", strict_slashes=False)
 def hello():
+    """hello route"""
     return "Hello HBNB!"
 
 
